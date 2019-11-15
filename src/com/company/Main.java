99343734e9612362631d4c05a4653399b4afd268
02connorkalinowski <48028356+02connorkalinowski@users.noreply.github.com>
2019-11-15 07:32:04 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,21 @@
 package com.company;
 
 public class Main {
+	public class Factorial
+{
+	public static void main(String[] args)
+	{	final int NUM_FACTS = 100;
+		for(int i = 0; i < NUM_FACTS; i++)
+			System.out.println( i + "! is " + factorial(i));
+	}
+	
+	public static int factorial(int n)
+	{	int result = 1;
+		for(int i = 2; i <= n; i++)
+			result *= i;
+		return result;
+	}
+}
     public static void countBlocks(int levels) {
         int total = 0;
         for (int i = 1; i <= levels; i++) {
@@ -19,5 +34,6 @@ public class Main {
             }
         }
         countBlocks(7);
+	factorial(i);
     }
 }
